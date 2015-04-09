fe838b739038a45638f6e368d30c48825084e28e
yingdongo <yingdongo@hotmail.com>
2015-04-09 21:31:14 +0200

    update

---
+++ feature_engineering.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 21:03:22 2015
+
+@author: Ying
+"""
+
+def feature_engineering(data):
+    data['temp_diff']=abs(data.atemp-data.temp)
+    return data
+    
